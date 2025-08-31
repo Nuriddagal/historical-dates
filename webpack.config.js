5c95ec1de7333863e3502e14aa40fd023eed68c5
Nuriddagal <creakgam@gmail.com>
2025-08-31 13:04:02 +0300

    changed publicPath

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    publicPath: '/',
+    publicPath: './',
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
